b68587643b176a5821d6367a927d9c59dd1bf608
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-03-27 18:58:39 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ from fastai.vision import open_image, load_learner, image, torch
 import torch
 
 def infer(img):
-    model = load_learner("model/")
+    model = cnn_learner(data, models.resnet50, metrics=[accuracy], model_dir = Path('../kaggle/working'),path = Path("."))
     pred_class = model.predict(img)[0]
     pred_prob = round(torch.max(model.predict(img)[2]).item()*100)
     
